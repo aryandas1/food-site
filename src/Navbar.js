44c4e39fe7f092cea9cd50b86b36daeed7db8b1d
Aryan Das <adas1@umd.edu>
2025-01-18 14:20:16 -0500

    shared password login

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,6 +1,6 @@
 import { Link } from 'react-router-dom';
 
-const Navbar = () => (
+const Navbar = ({ onLogout }) => (
   <nav className="bg-white-600 text-black p-6 flex justify-between items-center">
     <Link to="/" className="flex items-center">
       <img
@@ -10,6 +10,15 @@ const Navbar = () => (
       />
       <h1 className="text-2xl font-bold">AA Eats</h1>
     </Link>
+    <div>
+      <button
+        onClick={onLogout}
+        className="bg-red-500 text-white py-2 px-4 rounded hover:bg-red-600"
+      >
+        Logout
+      </button>
+    </div>
   </nav>
 );
+
 export default Navbar;
